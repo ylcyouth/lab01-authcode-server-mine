9551681d249baaba9e03c0ca66d877a8582f65ae
ylcyouth <yx_163_1@163.com>
2019-05-29 00:11:00 +0800

    1.编写获取用户信息的资源API

---
+++ src/main/java/com/ylcyouth/spring2go/authcodeserver/api/UserController.java
@@ -0,0 +1,35 @@
+package com.ylcyouth.spring2go.authcodeserver.api;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author wjj
+ * @create 2019/5/29 0:02
+ */
+@Controller
+public class UserController {
+
+    /**
+     * 资源API
+     */
+    @RequestMapping("/api/userinfo")
+    public ResponseEntity<UserInfo> getUserInfo() {
+
+        User user = (User) SecurityContextHolder.getContext()
+                .getAuthentication()
+                .getPrincipal();
+        String email = user.getUsername() + "@spring2go.com";
+
+        UserInfo userInfo = new UserInfo();
+        userInfo.setName(user.getUsername());
+        userInfo.setEmail(email);
+
+        return ResponseEntity.ok(userInfo);
+    }
+
+
+
+}
---
+++ src/main/java/com/ylcyouth/spring2go/authcodeserver/api/UserInfo.java
@@ -0,0 +1,28 @@
+package com.ylcyouth.spring2go.authcodeserver.api;
+
+/**
+ * @author wjj
+ * @create 2019/5/29 0:01
+ */
+public class UserInfo {
+
+    private String name;
+
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
