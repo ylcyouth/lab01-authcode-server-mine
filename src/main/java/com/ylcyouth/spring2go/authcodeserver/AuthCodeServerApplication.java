23b7530807befb297be5ecfbb0e7790f285c8ab1
ylcyouth <yx_163_1@163.com>
2019-05-29 00:00:05 +0800

    1.编写spring boot的启动类

---
+++ src/main/java/com/ylcyouth/spring2go/authcodeserver/AuthCodeServerApplication.java
@@ -0,0 +1,16 @@
+package com.ylcyouth.spring2go.authcodeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wjj
+ * @create 2019/5/28 23:36
+ */
+@SpringBootApplication
+public class AuthCodeServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthCodeServerApplication.class, args);
+    }
+}
